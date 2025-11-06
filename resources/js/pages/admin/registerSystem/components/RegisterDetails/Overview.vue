8a1cf7474f427b66eeb0425d7cc967ce37d24bde
Guenther Kron <kron@naturwelt.at>
2025-11-06 23:31:15 +0100

    toggleRegister 3.1.4

--- resources/js/pages/admin/registerSystem/components/RegisterDetails/Overview.vue
+++ resources/js/pages/admin/registerSystem/components/RegisterDetails/Overview.vue
@@ -19,14 +19,6 @@
                             <v-icon icon="mdi-power-standby" :color="selected_register.is_active ? 'success' : 'error'"></v-icon>
                         </v-btn>
                     </v-card-text>
-                    <v-card-text>
-                        selected_register
-                        {{ selected_register }}
-                    </v-card-text>
-                    <v-card-text class="mt-5">
-                        selected_active_register
-                        {{ active_registers }}
-                    </v-card-text>
                 </v-card>
             </its-grid-box>
         </v-col>
