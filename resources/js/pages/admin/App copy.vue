2df498c248808ae9d7c6c536b4297c03fe606aee
Guenther Kron <kron@naturwelt.at>
2025-11-07 00:03:56 +0100

    Loaded Methods 3.1.5

--- resources/js/pages/admin/App copy.vue
+++ resources/js/pages/admin/App copy.vue
@@ -11,22 +11,18 @@
             </v-toolbar>
             <v-list>
                 <template v-for="(item, i) in config.menu" :key="i">
-                    <!-- route item -->
-                    <v-list-item v-if="item.to" :exact="false" :title="item.title" :prepend-icon="item.icon" :to="item.to" />
-                    <!-- click item -->
-                    <v-list-item v-else-if="item.click" :exact="false" :title="item.title" :prepend-icon="item.icon" @click="callItemClick(item)" />
+                    <v-list-item :exact="false" :title="item.title" :prepend-icon="item.icon" v-if="item.to" :to="item.to" />
+                    <v-list-item :exact="false" v-if="item.click" :title="item.title" :prepend-icon="item.icon" @click="callItemClick(item)" />
                 </template>
             </v-list>
         </v-navigation-drawer>
 
         <v-app-bar flat color="primary" v-if="config && config.is_auth">
-            <template #prepend>
+            <template v-slot:prepend>
                 <v-btn icon="mdi-menu-open" v-if="!show_navigation_drawer" @click="show_navigation_drawer = true" />
                 <img :src="'/storage/images/' + config?.selected_school?.logo + '?t=' + Date.now()" alt="Logo" height="60px" class="pl-2" v-if="config?.selected_school?.logo" />
             </template>
-            <template #title>
-                {{ config?.selected_school?.long_name }}
-            </template>
+            <template v-slot:title>{{ config?.selected_school?.long_name }}</template>
         </v-app-bar>
 
         <v-main class="bg-background" v-if="config">
@@ -40,23 +36,32 @@
             </v-row>
         </v-footer>
 
-        <!-- loading overlay -->
+        <!-- Es wird aktuell etwas geladen-->
         <div class="d-flex justify-center align-center" style="position: fixed; inset: 0; background-color: rgba(255, 255, 255, 0.8); z-index: 9999" v-if="is_loading > 0">
             <v-progress-circular indeterminate size="70" width="7" />
         </div>
     </v-app>
 </template>
 
-<script>
-import axios from 'axios'
+<script setup>
 import ItsNotification from '@/pages/components/ItsNotification.vue'
-import { useAdminStore } from '@/stores/admin/AdminStore'
+</script>
+
+<script>
 import { mapWritableState } from 'pinia'
+import { useAdminStore } from '@/stores/admin/AdminStore'
 
 export default {
-    components: {
-        ItsNotification,
+    components: {},
+
+    async beforeMount() {
+        await axios.get('/sanctum/csrf-cookie')
+        this.adminStore = useAdminStore()
+        this.adminStore.initialize(this.$router)
+        await this.adminStore.loadConfig()
     },
+
+    unmounted() {},
 
     data() {
         return {
@@ -65,23 +70,11 @@ export default {
     },
 
     computed: {
-        // these will become this.config, this.is_loading, ...
         ...mapWritableState(useAdminStore, ['config', 'is_loading', 'show_navigation_drawer', 'load_config']),
-    },
-
-    async beforeMount() {
-        await axios.get('/sanctum/csrf-cookie')
-
-        // get pinia store and keep it on this
-        this.adminStore = useAdminStore()
-        this.adminStore.initialize(this.$router)
-
-        await this.adminStore.loadConfig()
     },
 
     methods: {
         async logout() {
-            // whatever your backend sequence is
             this.$router.push('/admin')
             await this.adminStore.executeLogout()
             await this.adminStore.loadConfig()
@@ -89,12 +82,8 @@ export default {
         },
 
         callItemClick(item) {
-            // item.click should be a string like "logout"
-            const fnName = item.click
-            if (fnName && typeof this[fnName] === 'function') {
-                this[fnName]()
-            } else {
-                console.warn('menu item click not found:', fnName)
+            if (item.click && typeof this[item.click] === 'function') {
+                this[item.click]()
             }
         },
     },
