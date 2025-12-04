e526a5819e7c476da4f8ced4bb1cbedca91387f4
Guenther Kron <kron@naturwelt.at>
2025-12-04 20:38:30 +0100

    Role teacher added

--- app/Console/Commands/AppUpdateCommand.php
+++ app/Console/Commands/AppUpdateCommand.php
@@ -33,7 +33,7 @@ class AppUpdateCommand extends Command
 
         // ✅ Roles and records
         $this->info('▶ ROLES AND RECORDS');
-        $service->createRoles(['super_admin', 'admin', 'register_admin', 'register_user', 'tutoring_user', 'tutoring_admin']);
+        $service->createRoles(['super_admin', 'admin', 'register_admin', 'register_user', 'tutoring_user', 'tutoring_admin', 'teacher']);
         $this->info('✅ Roles checked');
         $recordsCreateService->initRecords();
         $this->info('✅ Init Records checked');
