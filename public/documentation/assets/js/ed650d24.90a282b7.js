95ecbdb0f459f17daa816a453e020e800a1dc49e
Guenther Kron <kron@naturwelt.at>
2025-12-04 20:31:39 +0100

    Docu Initialized

---
+++ public/documentation/assets/js/ed650d24.90a282b7.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkschooltool=globalThis.webpackChunkschooltool||[]).push([[641],{8656:o=>{o.exports=JSON.parse('{"tags":[{"label":"Schooltool","permalink":"/documentation/blog/tags/schooltool","description":"Schooltool","count":1}]}')}}]);
