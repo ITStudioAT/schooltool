84015edd4afc629f763a237045b45f1810bbfede
Guenther Kron <kron@naturwelt.at>
2025-12-13 15:47:23 +0100

    SuperAdmin: See/Delete Log-File

--- config/schooltool.php
+++ config/schooltool.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'version' => '3.2.21',
+    'version' => '3.2.22',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
