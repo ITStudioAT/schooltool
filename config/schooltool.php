aab8ca465fcde0f8fa58a3774360cb2061474c05
Guenther Kron <kron@naturwelt.at>
2025-12-22 19:14:44 +0100

    Listed Teachers may Login

--- config/schooltool.php
+++ config/schooltool.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'version' => '3.4.3',
+    'version' => '3.4.4',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
