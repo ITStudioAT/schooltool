10ba90057e60bd724ebcaaa585f395d86462fa6a
Guenther Kron <kron@naturwelt.at>
2025-12-22 23:27:39 +0100

    Teacher can see Tutoring Offers

--- config/schooltool.php
+++ config/schooltool.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'version' => '3.4.4',
+    'version' => '3.4.5',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
