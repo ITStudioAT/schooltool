3fa15bb9bd4a07168e2347485fb3b9d806cff1b1
Guenther Kron <kron@naturwelt.at>
2025-12-23 20:08:29 +0100

    Login & Unkown Password

--- config/schooltool.php
+++ config/schooltool.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'version' => '3.4.5',
+    'version' => '3.5.0',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
