c88e8f913d337e4f674e790784da81d6a7716418
Guenther Kron <kron@naturwelt.at>
2025-12-31 07:35:59 +0100

    Tutoring Great Test Environment

--- config/schooltool.php
+++ config/schooltool.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'version' => '3.6.1',
+    'version' => '3.6.2',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
