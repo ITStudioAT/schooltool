eb9b4e193a400a9cca03d1d372aa9faf78f34f80
Guenther Kron <kron@naturwelt.at>
2026-01-08 23:38:02 +0100

    Admin: Deleting Register Dates

--- config/schooltool.php
+++ config/schooltool.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'version' => '3.6.2',
+    'version' => '3.6.3',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
