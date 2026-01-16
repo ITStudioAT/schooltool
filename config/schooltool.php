f759b2331080ddeacafb1f0d6cbdb9eff9fba90d
Guenther Kron <kron@naturwelt.at>
2026-01-16 17:13:46 +0100

    Secure Migration: Checking for double fields conflict

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.7.13',
+    'version' => '3.8.0',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
