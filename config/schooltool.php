fc1aecbece844d196ee2d4794441441bc92ff96e
Guenther Kron <kron@naturwelt.at>
2026-01-17 00:25:23 +0100

    Register: User: New UI-Design

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.9.0',
+    'version' => '3.9.1',
     'copyright' => '(c) 2025 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
