0e4d6df88708baabc534cea76cd7982e7d71b586
Guenther Kron <kron@naturwelt.at>
2026-02-15 00:55:54 +0100

    Teaching: Admin: Semester 1 Calulcation, when calculation grades

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.11.9',
+    'version' => '3.11.10',
     'copyright' => '(c) 2025–2026 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
