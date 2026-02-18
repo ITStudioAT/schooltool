a7273536672a87990f9c3dd143e50366572d1cea
Guenther Kron <kron@naturwelt.at>
2026-02-18 20:05:37 +0100

    Teaching: Overview Dates with Check, Overview Entries if Day

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.15.5',
+    'version' => '3.15.6',
     'copyright' => '(c) 2025–2026 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
