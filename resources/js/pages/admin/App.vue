577ef96f16c907b4e7454047c3cf84d91116d6e2
Guenther Kron <kron@naturwelt.at>
2026-02-26 22:32:12 +0100

    When Loading: Dashboard gets disbled

--- resources/js/pages/admin/App.vue
+++ resources/js/pages/admin/App.vue
@@ -11,13 +11,13 @@
             <v-list>
                 <template v-for="(item, i) in config.menu" :key="i">
                     <!-- route item -->
-                    <v-list-item v-if="item.to" :exact="false" :title="item.title" :prepend-icon="item.icon" :to="item.to" :disabled="is_navigation_locked || !item.is_active">
+                    <v-list-item v-if="item.to" :exact="false" :title="item.title" :prepend-icon="item.icon" :to="item.to" :disabled="is_navigation_locked || !item.is_active || is_loading > 0">
                         <template v-if="item.status_icon" #append>
                             <v-icon :icon="item.status_icon" :color="item.status_color || 'warning'" :title="item.status_title || ''" size="small" />
                         </template>
                     </v-list-item>
                     <!-- click item -->
-                    <v-list-item v-else-if="item.click" :exact="false" :title="item.title" :prepend-icon="item.icon" :disabled="is_navigation_locked" @click="callItemClick(item)" />
+                    <v-list-item v-else-if="item.click" :exact="false" :title="item.title" :prepend-icon="item.icon" :disabled="is_navigation_locked || is_loading > 0" @click="callItemClick(item)" />
                 </template>
             </v-list>
         </v-navigation-drawer>
--- resources/js/pages/admin/tutoring/Tutoring.vue
+++ resources/js/pages/admin/tutoring/Tutoring.vue
@@ -23,33 +23,43 @@
             </header>
 
             <!-- Menüleiste oben -->
-            <v-card
-                tile
-                flat
-                color="transparent"
-                class="d-flex flex-row ga-2 w-100 mb-2 super-admin-menu-row super-admin-menu-row--overview"
-                :class="{ 'is-disabled': action != '' || action_2 != '' }"
-                :disabled="action != '' || action_2 != ''">
-                <its-menu-button subtitle="Übersicht" icon="mdi-home" :color="main_action == 'overview' ? 'primary' : 'secondary'" @click="main_action = 'overview'" />
-                <its-menu-button
-                    subtitle="Einstellungen"
-                    icon="mdi-cog"
-                    :color="main_action == 'settings' ? 'primary' : 'secondary'"
-                    @click="main_action = 'settings'"
-                    v-if="config.roles.some((item) => admins.includes(item))" />
-                <its-menu-button
-                    subtitle="Fächer"
-                    icon="mdi-television-shimmer"
-                    :color="main_action == 'subjects' ? 'primary' : 'secondary'"
-                    @click="main_action = 'subjects'"
-                    v-if="config.roles.some((item) => admins.includes(item))" />
-                <its-menu-button
-                    subtitle="Benutzer"
-                    icon="mdi-account-multiple"
-                    :color="main_action == 'users' ? 'primary' : 'secondary'"
-                    @click="main_action = 'users'"
-                    v-if="config.roles.some((item) => admins.includes(item))" />
-            </v-card>
+            <div class="tutoring-admin-menu-wrap mb-2">
+                <v-card
+                    tile
+                    flat
+                    color="transparent"
+                    class="d-flex flex-row ga-2 w-100 super-admin-menu-row super-admin-menu-row--overview tutoring-admin-menu-row"
+                    :class="{ 'is-disabled': action != '' || action_2 != '' }"
+                    :disabled="action != '' || action_2 != ''">
+                    <its-menu-button
+                        subtitle="Übersicht"
+                        icon="mdi-home"
+                        :color="main_action == 'overview' ? 'primary' : 'secondary'"
+                        class="tutoring-admin-menu-button"
+                        @click="selectMainAction('overview')" />
+                    <its-menu-button
+                        subtitle="Einstellungen"
+                        icon="mdi-cog"
+                        :color="main_action == 'settings' ? 'primary' : 'secondary'"
+                        class="tutoring-admin-menu-button"
+                        @click="selectMainAction('settings')"
+                        v-if="config.roles.some((item) => admins.includes(item))" />
+                    <its-menu-button
+                        subtitle="Fächer"
+                        icon="mdi-television-shimmer"
+                        :color="main_action == 'subjects' ? 'primary' : 'secondary'"
+                        class="tutoring-admin-menu-button"
+                        @click="selectMainAction('subjects')"
+                        v-if="config.roles.some((item) => admins.includes(item))" />
+                    <its-menu-button
+                        subtitle="Benutzer"
+                        icon="mdi-account-multiple"
+                        :color="main_action == 'users' ? 'primary' : 'secondary'"
+                        class="tutoring-admin-menu-button"
+                        @click="selectMainAction('users')"
+                        v-if="config.roles.some((item) => admins.includes(item))" />
+                </v-card>
+            </div>
 
             <div class="super-admin-overview-shell super-admin-overview-shell--active tutoring-admin-shell">
                 <v-row class="w-100 ma-0" dense>
@@ -82,8 +92,6 @@ export default {
         this.adminStore = useAdminStore()
     },
 
-    unmounted() {},
-
     data() {
         return {
             adminStore: null,
@@ -93,10 +101,14 @@ export default {
     },
 
     computed: {
-        ...mapWritableState(useAdminStore, ['config', 'action', 'action_2']),
+        ...mapWritableState(useAdminStore, ['config', 'action', 'action_2', 'is_loading']),
     },
 
-    methods: {},
+    methods: {
+        selectMainAction(action) {
+            this.main_action = action
+        },
+    },
 }
 </script>
 
