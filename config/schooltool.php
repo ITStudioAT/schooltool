b09f00f228b5a775d3e5f044163b86cf0f095cfd
Guenther Kron <kron@naturwelt.at>
2026-02-26 22:43:28 +0100

    Major Update: UI-Design, Tutoring, Materials

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.18.3',
+    'version' => '3.19.0',
     'copyright' => '(c) 2025–2026 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
