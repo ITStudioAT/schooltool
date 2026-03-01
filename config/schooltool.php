11f98e528b21eaf7cb240b840ef4b2b066722945
Guenther Kron <kron@naturwelt.at>
2026-03-01 15:22:42 +0100

    Sharing Of Materials

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.19.2',
+    'version' => '3.20.0',
     'copyright' => '(c) 2025–2026 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
