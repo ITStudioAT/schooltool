53560dd9f0e8fe7ac3e679b341a231b1b0e9b037
Guenther Kron <kron@naturwelt.at>
2026-03-30 02:37:37 +0200

    ABA and more

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.28.1',
+    'version' => '3.29.0',
     'copyright' => '(c) 2025–2026 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
