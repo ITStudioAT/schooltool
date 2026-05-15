791d000f5977684115b3ce770992eb61d018bd18
Guenther Kron <kron@naturwelt.at>
2026-05-15 02:02:08 +0200

    Timetables, Curriculum, UI-Design

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.34.1',
+    'version' => '3.35.0',
     'copyright' => '(c) 2025 – 2026 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
