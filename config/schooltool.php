8bce755a06d04976015e77bad345a119c8a38425
Guenther Kron <kron@naturwelt.at>
2026-05-27 06:37:24 +0200

    StudentTimeTable Updates

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.36.9',
+    'version' => '3.36.10',
     'copyright' => '(c) 2025 – 2026 ITStudio.at by Günther Kron',
     'logo' => 'schooltool_white.png',
     'pagination' => 30,
