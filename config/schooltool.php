b0694f7df0aa6abe787c36d97ec52352adc56767
Guenther Kron <kron@naturwelt.at>
2026-08-04 10:40:00 +0300

    New Deploy

--- config/schooltool.php
+++ config/schooltool.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.44.4',
+    'version' => '3.44.6',
     'environment_versions' => [
         'composer' => env('APP_COMPOSER_VERSION'),
         'npm' => env('APP_NPM_VERSION'),
